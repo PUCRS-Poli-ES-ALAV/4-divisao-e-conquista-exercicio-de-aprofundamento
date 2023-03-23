0d2fb524add2d8ffa8b4b730c9ed795d5fafb97d
Michael da Costa Mora <michael.c.mora@gmail.com>
2023-03-23 18:39:46 -0300

    Ajustes depois do download

--- src/main/java/br/pucrs/sorts/Sorts.java
+++ src/main/java/br/pucrs/sorts/Sorts.java
@@ -5,9 +5,9 @@ import java.util.Random;
 import br.pucrs.util.ContagemRes;
 
 public class Sorts {
-	public long[] geraVetor(int nroElem, int lim){
+	public long[] geraVetor(int nroElem, int lim) {
 		int dummy;
-		long [] res = null;
+		long[] res = null;
 		int cont = 0;
 		Random rnd = new Random(System.nanoTime() * System.currentTimeMillis());
 
@@ -16,33 +16,32 @@ public class Sorts {
 			res = new long[nroElem];
 
 			while (cont < nroElem) {
-				dummy = rnd.nextInt(lim)+1;
-				dummy = rnd.nextInt(lim)+1;
-				
-				dummy = rnd.nextInt(lim)+1;
+				dummy = rnd.nextInt(lim) + 1;
+				dummy = rnd.nextInt(lim) + 1;
+
+				dummy = rnd.nextInt(lim) + 1;
 				res[cont++] = (long) dummy;
 			}
 		}
 		return res;
 	}
 
-	public long[] geraVetorInv(int nroElem){
-		long [] res = null;
-		
+	public long[] geraVetorInv(int nroElem) {
+		long[] res = null;
+
 		if (nroElem > 0) {
 
 			res = new long[nroElem];
 
-			for (int i = nroElem -1; i >= 0; i--)
+			for (int i = nroElem - 1; i >= 0; i--)
 				res[i] = (long) nroElem - i;
 		}
 		return res;
 	}
 
-
 	// Insira os métodos de sort aqui.
 
-	public ContagemRes bubbleSort0(long [] vet) {
+	public ContagemRes bubbleSort0(long[] vet) {
 		ContagemRes res = new ContagemRes();
 		long[] v = vet.clone();
 
@@ -54,8 +53,8 @@ public class Sorts {
 		return res;
 	}
 
-	private void bubbleSort0(long [] vet, ContagemRes contRes) {
-		long aux; 
+	private void bubbleSort0(long[] vet, ContagemRes contRes) {
+		long aux;
 		int i;
 
 		contRes.incrInstrucoes(1);
@@ -65,12 +64,12 @@ public class Sorts {
 				contRes.incrIteracoes(1);
 				contRes.incrInstrucoes(5);
 				contRes.incrInstrucoes(4);
-				if (vet[ i ] > vet[ i + 1 ]) {
+				if (vet[i] > vet[i + 1]) {
 					contRes.incrInstrucoes(9);
 					aux = vet[i];
-					vet[ i ] = vet[ i + 1 ];
+					vet[i] = vet[i + 1];
 					vet[i + 1] = aux;
-				}		         
+				}
 			}
 
 			if (i == 0)
@@ -78,48 +77,48 @@ public class Sorts {
 		}
 	}
 
-	public ContagemRes mergeSort(long [] vet)
-	{
+	public ContagemRes mergeSort(long[] vet) {
 		ContagemRes res = new ContagemRes();
-		long [] v = vet.clone();
+		long[] v = vet.clone();
 
 		res.resetCounters();
-		mergeSort(v, 0, v.length-1, res);
+		mergeSort(v, 0, v.length - 1, res);
 		res.getClockSec();
 		res.setResult(v);
 
 		return res;
 	}
 
-	private void mergeSort(long [] v, int inicio, int fim, ContagemRes contRes)
-	{
+	private void mergeSort(long[] v, int inicio, int fim, ContagemRes contRes) {
 		contRes.incrIteracoes(1);
 		contRes.incrInstrucoes(1);
-		if (inicio < fim)
-		{
+		if (inicio < fim) {
 			int meio = (inicio + fim) / 2;
 			mergeSort(v, inicio, meio, contRes);
-			mergeSort(v, meio+1, fim, contRes);
+			mergeSort(v, meio + 1, fim, contRes);
 			merge(v, inicio, meio, fim, contRes);
 			contRes.incrInstrucoes(7);
 		}
 	}
 
-	private void merge(long [] v, int inicio, int meio, int fim, ContagemRes contRes) {
-		int nL = meio-inicio+1; int nR = fim-meio;
-		long [] L = new long[nL]; long [] R = new long[nR];
+	private void merge(long[] v, int inicio, int meio, int fim, ContagemRes contRes) {
+		int nL = meio - inicio + 1;
+		int nR = fim - meio;
+		long[] L = new long[nL];
+		long[] R = new long[nR];
 
 		System.arraycopy(v, inicio, L, 0, nL);
 		contRes.incrIteracoes(nL);
 
-		System.arraycopy(v, meio+1, R, 0, nR);
+		System.arraycopy(v, meio + 1, R, 0, nR);
 		contRes.incrIteracoes(nR);
 
 		contRes.incrInstrucoes(10);
 
 		contRes.incrInstrucoes(1);
-		int iL = 0; int iR = 0;
-		for (int k=inicio; k<=fim; k++) {
+		int iL = 0;
+		int iR = 0;
+		for (int k = inicio; k <= fim; k++) {
 
 			contRes.incrIteracoes(1);
 			contRes.incrInstrucoes(4);
@@ -128,15 +127,15 @@ public class Sorts {
 				contRes.incrInstrucoes(1);
 				if (iL < nL) {
 					contRes.incrInstrucoes(7);
-					if (L[iL] < R[iR]) v[k] = L[iL++];
-					else v[k] = R[iR++];
-				}
-				else {
+					if (L[iL] < R[iR])
+						v[k] = L[iL++];
+					else
+						v[k] = R[iR++];
+				} else {
 					v[k] = R[iR++];
 					contRes.incrInstrucoes(5);
 				}
-			}
-			else {
+			} else {
 				v[k] = L[iL++];
 				contRes.incrInstrucoes(5);
 			}
