2046588bd40f5ca9b5ba929741128d26ece718b4
Guilherme Poletti <58012969+polettiguilherme@users.noreply.github.com>
2024-08-19 20:40:14 -0300

    exercicio-1

--- src/test/java/br/pucrs/AppTest.java
+++ src/test/java/br/pucrs/AppTest.java
@@ -1,20 +1,49 @@
 package br.pucrs;
 
-import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.Random;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testMergeSortWithRandomNumbers() {
+        ArrayList<Integer> array32 = generateRandomList(32);
+        ArrayList<Integer> sArray32 = App.mergeSort(array32);
+        System.out.println(App.recursionCount);
+        System.out.println(App.mergeTime);
+        assertTrue(isSorted(sArray32));
+
+        ArrayList<Integer> array2048 = generateRandomList(2048);
+        ArrayList<Integer> sArray2048 = App.mergeSort(array2048);
+        System.out.println(App.recursionCount);
+        System.out.println(App.mergeTime);
+        assertTrue(isSorted(sArray2048));
+
+        ArrayList<Integer> array1m = generateRandomList(1048576);
+        ArrayList<Integer> sArray1m = App.mergeSort(array1m);
+        System.out.println(App.recursionCount);
+        System.out.println(App.mergeTime);
+        assertTrue(isSorted(sArray1m));
+    }
+
+    private ArrayList<Integer> generateRandomList(Integer size) {
+        ArrayList<Integer> list = new ArrayList<>(size);
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            list.add(rand.nextInt(10000));
+        }
+        return list;
+    }
+
+    private boolean isSorted(ArrayList<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i - 1) > list.get(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
